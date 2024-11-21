bf15a396dc7531667d21473d32c56a1276650822
Sahand Safizadeh <sahands@systemgroup.net>
2024-11-21 16:03:40 +0330

    feat(tensor): move tinit to root tensor package

---
+++ tensor/types.go
@@ -0,0 +1,25 @@
+package tensor
+
+import "github.com/sahandsafizadeh/qeep/tensor/internal/tensor"
+
+type Tensor = tensor.Tensor
+type Range = tensor.Range
+
+type Device int
+
+const (
+	CPU Device = iota + 1
+)
+
+type Config struct {
+	Device    Device
+	GradTrack bool
+}
+
+type inputDataType interface {
+	float64 |
+		[]float64 |
+		[][]float64 |
+		[][][]float64 |
+		[][][][]float64
+}

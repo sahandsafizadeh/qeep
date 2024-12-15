5f611aac33f21f96489491cf457db62a81e457e8
Sahand Safizadeh <sahands@systemgroup.net>
2024-12-15 17:18:10 +0330

    fix: step

--- examples/01-boston-housing/main.go
+++ examples/01-boston-housing/main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
 func main() {
+	const dataFile = "data.csv"
+
+	file, err := os.Open(dataFile)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.Comma = ' '
+
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			panic(err)
+		}
+		fmt.Println(record)
+	}
 }

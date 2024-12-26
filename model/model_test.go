dd648ed38fa7545f9608dba03420d12181da8711
Sahand Safizadeh <sahands@systemgroup.net>
2024-12-26 18:59:59 +0330

    feat(model): add validation tests

--- model/model_test.go
+++ model/model_test.go
@@ -4,11 +4,13 @@ import (
 	"testing"
 
 	"github.com/sahandsafizadeh/qeep/component/layers"
+	"github.com/sahandsafizadeh/qeep/component/layers/activations"
 	"github.com/sahandsafizadeh/qeep/component/losses"
 	"github.com/sahandsafizadeh/qeep/component/optimizers"
 	"github.com/sahandsafizadeh/qeep/model"
 	"github.com/sahandsafizadeh/qeep/model/batchgens"
 	"github.com/sahandsafizadeh/qeep/model/stream"
+	"github.com/sahandsafizadeh/qeep/tensor"
 )
 
 func TestModel(t *testing.T) {
@@ -126,4 +128,170 @@ func prepareData() (trainBatchGen, testBatchGen model.BatchGenerator, err error)
 	}
 
 	return trainBatchGen, testBatchGen, nil
+}
+
+func TestValidationModel(t *testing.T) {
+	tensor.RunTestLogicOnDevices(func(_ tensor.Device) {
+
+		var conf *model.ModelConfig
+		var input *stream.Stream
+		var output *stream.Stream
+		var inputs []*stream.Stream
+
+		/* ------------------------------ */
+
+		conf = nil
+		input = nil
+		output = nil
+		inputs = []*stream.Stream{input}
+
+		_, err := model.NewMultiInputModel(inputs, output, conf)
+		if err == nil {
+			t.Fatalf("expected error because of nil input config")
+		} else if err.Error() != "Model config data validation failed: expected config not to be nil" {
+			t.Fatal("unexpected error message returned")
+		}
+
+		/* --------------- */
+
+		conf = new(model.ModelConfig)
+		output = nil
+		inputs = nil
+
+		_, err = model.NewMultiInputModel(inputs, output, conf)
+		if err == nil {
+			t.Fatalf("expected error because of not having at least one input stream")
+		} else if err.Error() != "Model input/output stream validation failed: expected to have at least one input stream" {
+			t.Fatal("unexpected error message returned")
+		}
+
+		/* --------------- */
+
+		conf = new(model.ModelConfig)
+		input = nil
+		output = nil
+		inputs = []*stream.Stream{input}
+
+		_, err = model.NewMultiInputModel(inputs, output, conf)
+		if err == nil {
+			t.Fatalf("expected error because of receiving nil input stream")
+		} else if err.Error() != "Model input/output stream validation failed: expected input stream at position (0) not to be nil" {
+			t.Fatal("unexpected error message returned")
+		}
+
+		/* --------------- */
+
+		conf = new(model.ModelConfig)
+		input = new(stream.Stream)
+		output = nil
+		inputs = []*stream.Stream{input}
+
+		_, err = model.NewMultiInputModel(inputs, output, conf)
+		if err == nil {
+			t.Fatalf("expected error because of receiving input stream which is not proparely initialized")
+		} else if err.Error() != "Model input/output stream validation failed: expected input stream at position (0) to be proparely initialized" {
+			t.Fatal("unexpected error message returned")
+		}
+
+		/* --------------- */
+
+		conf = new(model.ModelConfig)
+		input = stream.Tanh()()
+		output = nil
+		inputs = []*stream.Stream{input}
+
+		_, err = model.NewMultiInputModel(inputs, output, conf)
+		if err == nil {
+			t.Fatalf("expected error because of receiving input stream which does not contain layer of type 'Input'")
+		} else if err.Error() != "Model input/output stream validation failed: expected input stream at position (0) to contain layer of type 'Input'" {
+			t.Fatal("unexpected error message returned")
+		}
+
+		/* --------------- */
+
+		conf = new(model.ModelConfig)
+		input = stream.Input()
+		output = nil
+		inputs = []*stream.Stream{input}
+
+		_, err = model.NewMultiInputModel(inputs, output, conf)
+		if err == nil {
+			t.Fatalf("expected error because of receiving nil output stream")
+		} else if err.Error() != "Model input/output stream validation failed: expected output stream not to be nil" {
+			t.Fatal("unexpected error message returned")
+		}
+
+		/* --------------- */
+
+		conf = new(model.ModelConfig)
+		input = stream.Input()
+		output = new(stream.Stream)
+		inputs = []*stream.Stream{input}
+
+		_, err = model.NewMultiInputModel(inputs, output, conf)
+		if err == nil {
+			t.Fatalf("expected error because of receiving output stream which is not proparely initialized")
+		} else if err.Error() != "Model input/output stream validation failed: expected output stream to be proparely initialized" {
+			t.Fatal("unexpected error message returned")
+		}
+
+		/* --------------- */
+
+		conf = new(model.ModelConfig)
+		input = stream.Input()
+		output = stream.Softmax(&activations.SoftmaxConfig{
+			Dim: -1,
+		})(input)
+		inputs = []*stream.Stream{input}
+
+		_, err = model.NewMultiInputModel(inputs, output, conf)
+		if err == nil {
+			t.Fatalf("expected error because of receiving output stream which is not proparely initialized")
+		} else if err.Error() != `Model input/output stream validation failed: 
+(Layer 1): Softmax config data validation failed: expected 'Dim' not to be negative: got (-1)` {
+			t.Fatal("unexpected error message returned")
+		}
+
+		/* ------------------------------ */
+
+		conf = new(model.ModelConfig)
+		input = stream.Input()
+		output = stream.Softmax(nil)(input)
+		inputs = []*stream.Stream{input}
+
+		m, err := model.NewMultiInputModel(inputs, output, conf)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		/* --------------- */
+
+		err = m.Fit(nil, nil)
+		if err == nil {
+			t.Fatalf("expected error because of nil input config")
+		} else if err.Error() != "Fit config data validation failed: expected config not to be nil" {
+			t.Fatal("unexpected error message returned")
+		}
+
+		err = m.Fit(nil, &model.FitConfig{
+			Epochs: 0,
+		})
+		if err == nil {
+			t.Fatalf("expected error because of nil input config")
+		} else if err.Error() != "Fit config data validation failed: expected 'Epochs' to be positive: got (0)" {
+			t.Fatal("unexpected error message returned")
+		}
+
+		/* --------------- */
+
+		_, err = m.Predict([]tensor.Tensor{nil, nil})
+		if err == nil {
+			t.Fatalf("expected error because of the number of input tensors not being equal to model inputs")
+		} else if err.Error() != "Predict input data validation failed: expected exactly (1) input tensors: got (2)" {
+			t.Fatal("unexpected error message returned")
+		}
+
+		/* ------------------------------ */
+
+	})
 }

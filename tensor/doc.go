ad7d277688e3262d9a996d29c3e1b71b72eda08a
Sahand Safizadeh <sahands@systemgroup.net>
2025-01-02 15:34:32 +0330

    refactor: move docs

---
+++ tensor/doc.go
@@ -0,0 +1,11 @@
+package tensor
+
+/*
+Tensor is a data structure with variable number of dimensions that supports statistics and linear-algebra operations.
+By design:
+- Tensor is an interface so that it can be implemented on various devices internally.
+- Tensor's implementation must be immutable
+- Tensor's implementation must support automatic gradient calculation using `gradtrack` package.
+- Every Tensor must have a valid gradient state or `GradContext`; it is the only part of tensors which is not immutable.
+- After back-propagation, state of GradContext is not valid and it must be reset.
+*/

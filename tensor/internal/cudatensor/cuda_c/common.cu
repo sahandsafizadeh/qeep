38f4eafc3704f1cb753c2e52fc6f865b066fa4ac
Sahand Safizadeh <sahands@systemgroup.net>
2025-03-30 00:44:03 +0330

    feat(cudatensor): complete common utilities

--- tensor/internal/cudatensor/cuda_c/common.cu
+++ tensor/internal/cudatensor/cuda_c/common.cu
@@ -2,20 +2,22 @@
 #include <stdio.h>
 #include <math.h>
 
+#include "common.h"
+
+/* ----- kernel launch ----- */
+
 const int MAX_THREADS_PER_BLOCK_X = 512;
 const int MAX_BLOCKS_PER_GRID_X = 65535;
 
-int threadSize(size_t n)
+LaunchParams launchParams(size_t n)
 {
-    return MAX_THREADS_PER_BLOCK_X;
+    int threads = MAX_THREADS_PER_BLOCK_X;
+    int blocks = (n + threads - 1) / threads;
+    blocks = min(blocks, MAX_BLOCKS_PER_GRID_X);
+    return (LaunchParams){threads, blocks};
 }
 
-int blockSize(size_t n)
-{
-    int threads = threadSize(n);
-    int blocks = (n + threads - 1) / threads;
-    return min(blocks, MAX_BLOCKS_PER_GRID_X);
-}
+/* ----- error handling ----- */
 
 void handleCudaError(cudaError_t err)
 {
--- tensor/internal/cudatensor/cuda_c/common.h
+++ tensor/internal/cudatensor/cuda_c/common.h
@@ -1,11 +1,19 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stddef.h>
+#include <cuda_runtime.h>
+
+typedef struct LaunchParams
+{
+    int blockSize;
+    int threadSize;
+};
+
 typedef double(scalarUnaryFunc)(double);
 typedef double(scalarBinaryFunc)(double, double);
 
-int blockSize(size_t n);
-int threadSize(size_t n);
+LaunchParams launchParams(size_t n);
 void handleCudaError(cudaError_t err);
 
 #endif // COMMON_H

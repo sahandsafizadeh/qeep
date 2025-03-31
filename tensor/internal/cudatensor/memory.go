946bc23849a77547d38f465ce5e8a38600e3787e
Sahand Safizadeh <sahands@systemgroup.net>
2025-03-31 10:45:43 +0330

    feat: add memory file

---
+++ tensor/internal/cudatensor/memory.go
@@ -0,0 +1,32 @@
+package cudatensor
+
+/*
+   #cgo LDFLAGS: -L. -lcudatensor
+   #include "cuda_c/cudatensor.h"
+*/
+import "C"
+
+import (
+	"runtime"
+	"unsafe"
+)
+
+func newCUDATensor(dims []int, data *C.double) (t *CUDATensor) {
+	tdims := make([]int, len(dims))
+	tdata := unsafe.Pointer(data)
+	copy(tdims, dims)
+
+	t = &CUDATensor{
+		dims: tdims,
+		data: tdata,
+	}
+
+	runtime.GC()
+	runtime.AddCleanup(&t, freeCUDATensorData, data)
+
+	return t
+}
+
+func freeCUDATensorData(data *C.double) {
+	C.FreeCudaMem(data)
+}

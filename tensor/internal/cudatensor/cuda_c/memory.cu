668d911f29d4460245dca5dea762fc4b7e21aaae
Sahand Safizadeh <sahands@systemgroup.net>
2025-04-04 18:02:06 +0330

    fix: step

--- tensor/internal/cudatensor/cuda_c/memory.cu
+++ tensor/internal/cudatensor/cuda_c/memory.cu
@@ -8,10 +8,12 @@ extern "C"
 
 void GetCudaMemInfo(size_t *free_mem, size_t *total_mem)
 {
-    cudaMemGetInfo(free_mem, total_mem);
+    handleCudaError(
+        cudaMemGetInfo(free_mem, total_mem));
 }
 
 void FreeCudaMem(double *data)
 {
-    cudaFree(data);
+    handleCudaError(
+        cudaFree(data));
 }

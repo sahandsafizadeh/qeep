73907bc3048e60c72c225175c4db1bbf532dfdbe
Sahand Safizadeh <sahands@systemgroup.net>
2025-04-11 21:16:54 +0330

    feat(cudatensor): add slice accessor

--- tensor/internal/cudatensor/cuda_c/accessors.cu
+++ tensor/internal/cudatensor/cuda_c/accessors.cu
@@ -1,3 +1,4 @@
+#include "types.h"
 #include "common.h"
 
 /* ----- indexing functions ----- */
@@ -24,7 +25,7 @@ void rcumprod(int *rcp, const Range *range, size_t n)
     }
 }
 
-void encode(int *lnpos, const int *index, const int *rcp, size_t n)
+__host__ __device__ void encode(int *lnpos, const int *index, const int *rcp, size_t n)
 {
     *lnpos = 0;
     for (size_t i = 0; i < n; i++)
@@ -34,18 +35,122 @@ void encode(int *lnpos, const int *index, const int *rcp, size_t n)
     }
 }
 
+__host__ __device__ void decode(int *index, int lnpos, const int *rcp, size_t n)
+{
+    int rem = lnpos;
+    for (size_t i = 0; i < n; i++)
+    {
+        index[i] = rem / rcp[i];
+        rem %= rcp[i];
+    }
+}
+
+__host__ __device__ bool fallsin(const int *index, const Range *range, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        int idx = index[i];
+        int from = range[i].from;
+        int to = range[i].to;
+
+        if (!(from <= idx && idx < to))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 /* ----- helper functions ----- */
 
-int linearPosition(const int *dims, const int *index, size_t n)
+int elemsCount(const int *dims, size_t n)
 {
-    int lnpos = 0;
-    int *rcp = (int *)(malloc(n * sizeof(int)));
+    int count = 1;
+    for (size_t i = 0; i < n; i++)
+    {
+        count *= dims[i];
+    }
 
-    rcumprod(rcp, dims, n);
-    encode(&lnpos, index, rcp, n);
-    free(rcp);
+    return count;
+}
 
-    return lnpos;
+int elemsCount(const Range *range, size_t n)
+{
+    int count = 1;
+    for (size_t i = 0; i < n; i++)
+    {
+        count *= range[i].to - range[i].from;
+    }
+
+    return count;
+}
+
+/* ----- device functions ----- */
+
+__device__ inline unsigned int threadPosition()
+{
+    return threadIdx.x + blockIdx.x * blockDim.x;
+}
+
+__device__ inline unsigned int totalThreads()
+{
+    return gridDim.x * blockDim.x;
+}
+
+__device__ int transformPosition(
+    int lnpos_src,
+    const int *rcp_src,
+    const int *rcp_dst,
+    const Range *range,
+    size_t n)
+{
+    int lnpos_dst;
+    int *index_src = (int *)(malloc(n * sizeof(int)));
+    int *index_dst = (int *)(malloc(n * sizeof(int)));
+
+    decode(index_src, lnpos_src, rcp_src, n);
+
+    if (!fallsin(index_src, range, n))
+    {
+        return -1;
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+        index_dst[i] = index_src[i] - range[i].from;
+    }
+
+    encode(&lnpos_dst, index_dst, rcp_dst, n);
+
+    free(index_src);
+    free(index_dst);
+
+    return lnpos_dst;
+}
+
+__global__ void copySlice(
+    double *dst,
+    const double *src,
+    size_t n_src,
+    const int *rcp_src,
+    const int *rcp_dst,
+    const Range *range,
+    size_t n_index)
+{
+    const unsigned int tpos = threadPosition();
+    const unsigned int stride = totalThreads();
+
+    for (size_t i = tpos; i < n_src; i += stride)
+    {
+        int lnpos_src = i;
+        int lnpos_dst = transformPosition(lnpos_src, rcp_src, rcp_dst, range, n_index);
+
+        if (lnpos_dst >= 0)
+        {
+            dst[lnpos_dst] = src[lnpos_src];
+        }
+    }
 }
 
 /* ----- API functions ----- */
@@ -53,11 +158,17 @@ int linearPosition(const int *dims, const int *index, size_t n)
 extern "C"
 {
     double At(const double *data, const int *dims, const int *index, size_t n);
+    double *Slice(const double *src, const int *dims, const Range *index, size_t n);
 }
 
 double At(const double *data, const int *dims, const int *index, size_t n)
 {
-    int lnpos = linearPosition(dims, index, n);
+    int lnpos;
+    int *rcp = (int *)(malloc(n * sizeof(int)));
+
+    rcumprod(rcp, dims, n);
+    encode(&lnpos, index, rcp, n);
+    free(rcp);
 
     double elem;
     handleCudaError(
@@ -68,4 +179,35 @@ double At(const double *data, const int *dims, const int *index, size_t n)
             cudaMemcpyDeviceToHost));
 
     return elem;
+}
+
+double *Slice(const double *src, const int *dims, const Range *index, size_t n)
+{
+    int n_index = n;
+    int n_src = elemsCount(dims, n_index);
+    int n_dst = elemsCount(index, n_index);
+
+    int *rcp_src = (int *)(malloc(n_index * sizeof(int)));
+    int *rcp_dst = (int *)(malloc(n_index * sizeof(int)));
+    rcumprod(rcp_src, dims, n_index);
+    rcumprod(rcp_dst, index, n_index);
+
+    double *dst;
+    handleCudaError(
+        cudaMalloc(&dst, n_dst * sizeof(double)));
+
+    LaunchParams lps = launchParams(n_src);
+
+    copySlice<<<lps.blockSize, lps.threadSize>>>(dst, src, n_src,
+                                                 rcp_src, rcp_dst, index, n_index);
+
+    handleCudaError(
+        cudaGetLastError());
+    handleCudaError(
+        cudaDeviceSynchronize());
+
+    free(rcp_src);
+    free(rcp_dst);
+
+    return dst;
 }

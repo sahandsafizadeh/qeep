8c0d5c5878a85a29b0f4033ea0b42918b4d2788d
Sahand Safizadeh <sahands@systemgroup.net>
2025-04-17 12:59:49 +0330

    feat(cudatensor): add operators cu

--- tensor/internal/cudatensor/cuda_c/cudatensor.h
+++ tensor/internal/cudatensor/cuda_c/cudatensor.h
@@ -1,25 +1,47 @@
 #ifndef CUDATENSOR_H
 #define CUDATENSOR_H
 
+/*---------------- types ------------------*/
 #include <stddef.h>
 #include "types.h"
 
+/*------------- initializers --------------*/
 double *Full(size_t n, double value);
 double *Eye(size_t n, size_t d);
 double *RandU(size_t n, double l, double u);
 double *RandN(size_t n, double u, double s);
 double *Of(const double *input_data, size_t n);
 
+/*--------------- accessors ---------------*/
 double At(const double *data, const int *dims, const int *index, size_t n);
 double *Slice(const double *src, const int *dims, const Range *index, size_t n);
 double *Patch(const double *bas, const int *dims, const double *src, const Range *index, size_t n);
 
+/*--------------- operators ---------------*/
 double *Scale(const double *x, size_t n, double a);
 double *Pow(const double *x, size_t n, double a);
 double *Exp(const double *x, size_t n);
+double *Log(const double *x, size_t n);
+double *Sin(const double *x, size_t n);
+double *Cos(const double *x, size_t n);
+double *Tan(const double *x, size_t n);
+double *Sinh(const double *x, size_t n);
+double *Cosh(const double *x, size_t n);
+double *Tanh(const double *x, size_t n);
+double *Eq(const double *a, const double *b, size_t n);
+double *Ne(const double *a, const double *b, size_t n);
+double *Gt(const double *a, const double *b, size_t n);
+double *Ge(const double *a, const double *b, size_t n);
+double *Lt(const double *a, const double *b, size_t n);
+double *Le(const double *a, const double *b, size_t n);
+double *ElMin(const double *a, const double *b, size_t n);
+double *ElMax(const double *a, const double *b, size_t n);
 double *Add(const double *a, const double *b, size_t n);
+double *Sub(const double *a, const double *b, size_t n);
 double *Mul(const double *a, const double *b, size_t n);
+double *Div(const double *a, const double *b, size_t n);
 
+/*--------------- memory -----------------*/
 void GetCudaMemInfo(size_t *free_mem, size_t *total_mem);
 void FreeCudaMem(double *data);
 
--- tensor/internal/cudatensor/cuda_c/operators.cu
+++ tensor/internal/cudatensor/cuda_c/operators.cu
@@ -121,61 +121,117 @@ double div_(double a, double b)
 typedef double(scalarUnaryFunc)(double);
 typedef double(scalarBinaryFunc)(double, double);
 
-__device__ inline int threadPosition()
+__device__ inline unsigned int threadPosition()
 {
     return threadIdx.x + blockIdx.x * blockDim.x;
 }
 
-__device__ inline int totalThreads()
+__device__ inline unsigned int totalThreads()
 {
     return gridDim.x * blockDim.x;
 }
 
 __global__ void applyUnaryFuncElemWise(
-    scalarUnaryFunc suf,
-    const double *x,
+    double *dst,
+    const double *src,
     size_t n,
-    double *y)
+    scalarUnaryFunc suf)
 {
-    const int tpos = threadPosition();
-    const int stride = totalThreads();
+    const unsigned int tpos = threadPosition();
+    const unsigned int stride = totalThreads();
 
     for (size_t i = tpos; i < n; i += stride)
     {
-        y[i] = suf(x[i]);
+        dst[i] = suf(src[i]);
     }
 }
 
 __global__ void applyBinaryFuncElemWise(
-    scalarBinaryFunc sbf,
-    const double *a,
-    const double *b,
+    double *dst,
+    const double *src1,
+    const double *src2,
     size_t n,
-    double *c)
+    scalarBinaryFunc sbf)
 {
-    const int tpos = threadPosition();
-    const int stride = totalThreads();
+    const unsigned int tpos = threadPosition();
+    const unsigned int stride = totalThreads();
 
     for (size_t i = tpos; i < n; i += stride)
     {
-        c[i] = sbf(a[i], b[i]);
+        dst[i] = sbf(src1[i], src2[i]);
     }
 }
 
 __global__ void applyHalfBinaryFuncElemWise(
-    scalarBinaryFunc sbf,
-    const double *x,
+    double *dst,
+    const double *src1,
     size_t n,
-    double a,
-    double *y)
+    double srcc,
+    scalarBinaryFunc sbf)
 {
-    const int tpos = threadPosition();
-    const int stride = totalThreads();
+    const unsigned int tpos = threadPosition();
+    const unsigned int stride = totalThreads();
 
     for (size_t i = tpos; i < n; i += stride)
     {
-        y[i] = sbf(x[i], a);
+        dst[i] = sbf(src1[i], srcc);
     }
+}
+
+/* ----- API helper functions ----- */
+
+double *runHalfBinaryOp(const double *x, size_t n, double a, scalarBinaryFunc sbf)
+{
+    double *y;
+    handleCudaError(
+        cudaMalloc(&y, n * sizeof(double)));
+
+    LaunchParams lps = launchParams(n);
+
+    applyHalfBinaryFuncElemWise<<<lps.blockSize, lps.threadSize>>>(y, x, n, a, sbf);
+
+    handleCudaError(
+        cudaGetLastError());
+    handleCudaError(
+        cudaDeviceSynchronize());
+
+    return y;
+}
+
+double *runUnaryOp(const double *x, size_t n, scalarUnaryFunc suf)
+{
+    double *y;
+    handleCudaError(
+        cudaMalloc(&y, n * sizeof(double)));
+
+    LaunchParams lps = launchParams(n);
+
+    applyUnaryFuncElemWise<<<lps.blockSize, lps.threadSize>>>(y, x, n, suf);
+
+    handleCudaError(
+        cudaGetLastError());
+    handleCudaError(
+        cudaDeviceSynchronize());
+
+    return y;
+}
+
+double *runBinaryOp(const double *a, const double *b, size_t n, scalarBinaryFunc sbf)
+{
+    double *c;
+    handleCudaError(
+        cudaMalloc(&c, n * sizeof(double)));
+
+    LaunchParams lps = launchParams(n);
+
+    applyBinaryFuncElemWise<<<lps.blockSize, lps.threadSize>>>(c, a, b, n, sbf);
+
+    handleCudaError(
+        cudaGetLastError());
+    handleCudaError(
+        cudaDeviceSynchronize());
+
+    return c;
 }
 
 /* ----- API functions ----- */
@@ -185,96 +241,133 @@ extern "C"
     double *Scale(const double *x, size_t n, double a);
     double *Pow(const double *x, size_t n, double a);
     double *Exp(const double *x, size_t n);
+    double *Log(const double *x, size_t n);
+    double *Sin(const double *x, size_t n);
+    double *Cos(const double *x, size_t n);
+    double *Tan(const double *x, size_t n);
+    double *Sinh(const double *x, size_t n);
+    double *Cosh(const double *x, size_t n);
+    double *Tanh(const double *x, size_t n);
+    double *Eq(const double *a, const double *b, size_t n);
+    double *Ne(const double *a, const double *b, size_t n);
+    double *Gt(const double *a, const double *b, size_t n);
+    double *Ge(const double *a, const double *b, size_t n);
+    double *Lt(const double *a, const double *b, size_t n);
+    double *Le(const double *a, const double *b, size_t n);
+    double *ElMin(const double *a, const double *b, size_t n);
+    double *ElMax(const double *a, const double *b, size_t n);
     double *Add(const double *a, const double *b, size_t n);
+    double *Sub(const double *a, const double *b, size_t n);
     double *Mul(const double *a, const double *b, size_t n);
+    double *Div(const double *a, const double *b, size_t n);
 }
 
 double *Scale(const double *x, size_t n, double a)
 {
-    double *y;
-    handleCudaError(
-        cudaMalloc(&y, n * sizeof(double)));
-
-    LaunchParams lps = launchParams(n);
-
-    applyHalfBinaryFuncElemWise<<<lps.blockSize, lps.threadSize>>>(scale_, x, n, a, y);
-
-    handleCudaError(
-        cudaGetLastError());
-    handleCudaError(
-        cudaDeviceSynchronize());
-
-    return y;
+    return runHalfBinaryOp(x, n, a, scale_);
 }
 
 double *Pow(const double *x, size_t n, double a)
 {
-    double *y;
-    handleCudaError(
-        cudaMalloc(&y, n * sizeof(double)));
-
-    LaunchParams lps = launchParams(n);
-
-    applyHalfBinaryFuncElemWise<<<lps.blockSize, lps.threadSize>>>(pow_, x, n, a, y);
-
-    handleCudaError(
-        cudaGetLastError());
-    handleCudaError(
-        cudaDeviceSynchronize());
-
-    return y;
+    return runHalfBinaryOp(x, n, a, pow_);
 }
 
 double *Exp(const double *x, size_t n)
 {
-    double *y;
-    handleCudaError(
-        cudaMalloc(&y, n * sizeof(double)));
+    return runUnaryOp(x, n, exp_);
+}
 
-    LaunchParams lps = launchParams(n);
+double *Log(const double *x, size_t n)
+{
+    return runUnaryOp(x, n, log_);
+}
 
-    applyUnaryFuncElemWise<<<lps.blockSize, lps.threadSize>>>(exp_, x, n, y);
+double *Sin(const double *x, size_t n)
+{
+    return runUnaryOp(x, n, sin_);
+}
 
-    handleCudaError(
-        cudaGetLastError());
-    handleCudaError(
-        cudaDeviceSynchronize());
+double *Cos(const double *x, size_t n)
+{
+    return runUnaryOp(x, n, cos_);
+}
 
-    return y;
+double *Tan(const double *x, size_t n)
+{
+    return runUnaryOp(x, n, tan_);
+}
+
+double *Sinh(const double *x, size_t n)
+{
+    return runUnaryOp(x, n, sinh_);
+}
+
+double *Cosh(const double *x, size_t n)
+{
+    return runUnaryOp(x, n, cosh_);
+}
+
+double *Tanh(const double *x, size_t n)
+{
+    return runUnaryOp(x, n, tanh_);
+}
+
+double *Eq(const double *a, const double *b, size_t n)
+{
+    return runBinaryOp(a, b, n, eq_);
+}
+
+double *Ne(const double *a, const double *b, size_t n)
+{
+    return runBinaryOp(a, b, n, ne_);
+}
+
+double *Gt(const double *a, const double *b, size_t n)
+{
+    return runBinaryOp(a, b, n, gt_);
+}
+
+double *Ge(const double *a, const double *b, size_t n)
+{
+    return runBinaryOp(a, b, n, ge_);
+}
+
+double *Lt(const double *a, const double *b, size_t n)
+{
+    return runBinaryOp(a, b, n, lt_);
+}
+
+double *Le(const double *a, const double *b, size_t n)
+{
+    return runBinaryOp(a, b, n, le_);
+}
+
+double *ElMin(const double *a, const double *b, size_t n)
+{
+    return runBinaryOp(a, b, n, elmin_);
+}
+
+double *ElMax(const double *a, const double *b, size_t n)
+{
+    return runBinaryOp(a, b, n, elmax_);
 }
 
 double *Add(const double *a, const double *b, size_t n)
 {
-    double *c;
-    handleCudaError(
-        cudaMalloc(&c, n * sizeof(double)));
+    return runBinaryOp(a, b, n, add_);
+}
 
-    LaunchParams lps = launchParams(n);
-
-    applyBinaryFuncElemWise<<<lps.blockSize, lps.threadSize>>>(add_, a, b, n, c);
-
-    handleCudaError(
-        cudaGetLastError());
-    handleCudaError(
-        cudaDeviceSynchronize());
-
-    return c;
+double *Sub(const double *a, const double *b, size_t n)
+{
+    return runBinaryOp(a, b, n, sub_);
 }
 
 double *Mul(const double *a, const double *b, size_t n)
 {
-    double *c;
-    handleCudaError(
-        cudaMalloc(&c, n * sizeof(double)));
+    return runBinaryOp(a, b, n, mul_);
+}
 
-    LaunchParams lps = launchParams(n);
-
-    applyBinaryFuncElemWise<<<lps.blockSize, lps.threadSize>>>(mul_, a, b, n, c);
-
-    handleCudaError(
-        cudaGetLastError());
-    handleCudaError(
-        cudaDeviceSynchronize());
-
-    return c;
+double *Div(const double *a, const double *b, size_t n)
+{
+    return runBinaryOp(a, b, n, div_);
 }

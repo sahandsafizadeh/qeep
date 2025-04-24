3dfaafd013000c284b65c2dacd54c4a75e5cffae
Sahand Safizadeh <sahands@systemgroup.net>
2025-04-24 23:32:51 +0330

    feat: add more utils

---
+++ tensor/internal/util/shape_modifiers.go
@@ -0,0 +1,46 @@
+package util
+
+func UnSqueezeDims(dim int, dims []int) (res []int) {
+	left := dims[:dim]
+	res = make([]int, len(left))
+	copy(res, left)
+	res = append(res, 1)
+
+	lastDim := len(dims)
+	if dim < lastDim {
+		right := dims[dim:]
+		res = append(res, right...)
+	}
+
+	return res
+}
+
+func SqueezeDims(dim int, dims []int) (res []int) {
+	left := dims[:dim]
+	res = make([]int, len(left))
+	copy(res, left)
+
+	lastDim := len(dims) - 1
+	if dim < lastDim {
+		right := dims[dim+1:]
+		res = append(res, right...)
+	}
+
+	return res
+}
+
+func FlattenDims(dim int, dims []int) (res []int) {
+	left := dims[:dim]
+	res = make([]int, len(left))
+	copy(res, left)
+
+	nElems := 1
+
+	for i := dim; i < len(dims); i++ {
+		nElems *= dims[i]
+	}
+
+	res = append(res, nElems)
+
+	return res
+}

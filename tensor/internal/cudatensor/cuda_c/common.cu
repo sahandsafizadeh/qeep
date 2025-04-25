a4f58ef162f226ed099deed240508d3fd2299bd9
Sahand Safizadeh <sahands@systemgroup.net>
2025-04-25 12:40:22 +0330

    fix: step

--- tensor/internal/cudatensor/cuda_c/common.cu
+++ tensor/internal/cudatensor/cuda_c/common.cu
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 
+#include "types.h"
 #include "common.h"
 
 /* ----- kernel launch ----- */
@@ -25,4 +26,64 @@ void handleCudaError(cudaError_t err)
                cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
+}
+
+/* ----- indexing ----- */
+
+int elemcnt(DimArr dims)
+{
+    int count = 1;
+    for (size_t i = 0; i < dims.size; i++)
+    {
+        count *= dims.arr[i];
+    }
+
+    return count;
+}
+
+int elemcnt(RangeArr ranges)
+{
+    int count = 1;
+    for (size_t i = 0; i < ranges.size; i++)
+    {
+        Range range = ranges.arr[i];
+        count *= range.to - range.from;
+    }
+
+    return count;
+}
+
+DimArr rcumprod(DimArr dims)
+{
+    DimArr rcp;
+
+    int prod = 1;
+    for (size_t i = 0; i < dims.size; i++)
+    {
+        size_t j = dims.size - i - 1;
+        rcp.arr[j] = prod;
+        prod *= dims.arr[j];
+    }
+
+    rcp.size = dims.size;
+
+    return rcp;
+}
+
+DimArr rcumprod(RangeArr ranges)
+{
+    DimArr rcp;
+
+    int prod = 1;
+    for (size_t i = 0; i < ranges.size; i++)
+    {
+        size_t j = ranges.size - i - 1;
+        Range range = ranges.arr[j];
+        rcp.arr[j] = prod;
+        prod *= range.to - range.from;
+    }
+
+    rcp.size = ranges.size;
+
+    return rcp;
 }

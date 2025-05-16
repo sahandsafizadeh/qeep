eef71afc16ca85c0496dc0fc05aecaf18dca7bcf
Sahand Safizadeh <sahands@systemgroup.net>
2025-05-16 19:53:41 +0330

    feat(cudatensor): optimize garbage collection

--- tensor/internal/cudatensor/memory.go
+++ tensor/internal/cudatensor/memory.go
@@ -7,10 +7,22 @@ package cudatensor
 import "C"
 
 import (
+	"math/rand/v2"
 	"runtime"
 	"unsafe"
 
 	"github.com/sahandsafizadeh/qeep/tensor/internal/util"
+)
+
+const (
+	allocatedMemThreshold = 0.75
+	memInfoReloadRate     = 1e-4
+	doubleSizeBytes       = 8
+)
+
+var (
+	cudaTotalMem int64 = 0
+	cudaAllocMem int64 = 0
 )
 
 func newCUDATensor(dims []int, data *C.double) (t *CUDATensor) {
@@ -25,7 +37,10 @@ func newCUDATensor(dims []int, data *C.double) (t *CUDATensor) {
 		data: tdata,
 	}
 
-	runtime.AddCleanup(t, freeCUDATensorData, data)
+	arg := getCudaDataOf(t)
+	updateCudaAllocMem(t.n, +1)
+	runtime.AddCleanup(t, freeCUDATensorData, arg)
+
 	if enforceCleanup() {
 		runtime.GC()
 	}
@@ -33,10 +48,56 @@ func newCUDATensor(dims []int, data *C.double) (t *CUDATensor) {
 	return t
 }
 
-func freeCUDATensorData(data *C.double) {
-	C.FreeCudaMem(data)
+func freeCUDATensorData(cd C.CudaData) {
+	C.FreeCudaMem(cd.arr)
+	updateCudaAllocMem(int(cd.size), -1)
+}
+
+func updateCudaAllocMem(n int, dir int) {
+	updatedBytes := int64(n) * doubleSizeBytes
+	if dir > 0 {
+		cudaAllocMem += updatedBytes
+	} else {
+		cudaAllocMem -= updatedBytes
+	}
+
+	if cudaAllocMem < 0 {
+		panic("reached negative value for cudaAllocMem")
+	}
 }
 
 func enforceCleanup() bool {
-	return true
+	reloadCudaMemInfo()
+	allocMem := float64(cudaAllocMem)
+	totalMem := float64(cudaTotalMem)
+	return allocMem/totalMem > allocatedMemThreshold
+}
+
+func reloadCudaMemInfo() {
+	if !memInfoInitialized() || shouldReloadByChance() {
+		var freemem C.size_t
+		var totalmem C.size_t
+
+		freeptr := unsafe.Pointer(&freemem)
+		totalptr := unsafe.Pointer(&totalmem)
+
+		free_mem_c := (*C.size_t)(freeptr)
+		total_mem_c := (*C.size_t)(totalptr)
+
+		C.GetCudaMemInfo(free_mem_c, total_mem_c)
+
+		cudaFreeMem := int64(freemem)
+		cudaTotalMem = int64(totalmem)
+		cudaAllocMem = cudaTotalMem - cudaFreeMem
+	}
+}
+
+/* ----- helpers ----- */
+
+func memInfoInitialized() bool {
+	return cudaTotalMem > 0
+}
+
+func shouldReloadByChance() bool {
+	return rand.Float64() < memInfoReloadRate
 }

dd4904c6fd6dba94fe773b7769b4a5e990270cce
Sahand Safizadeh <sahand.safizadeh@xebia.com>
2026-05-30 22:07:41 +0200

    step

--- model/stream/stream.go
+++ model/stream/stream.go
@@ -41,7 +41,7 @@ func (s *Stream) Error() error {
 	return errors.New(chained.String())
 }
 
-func NewStream(initFunc layerInitFunc, xs []*Stream) (y *Stream) {
+func NewStream(initFunc layerInitFunc, xs []*Stream) *Stream {
 	/*
 		CHANGE WITH CAUTION:
 		- this function only handles error of 'initFunc'.

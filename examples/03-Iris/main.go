eb11e2dfcf2c35675198c231fb1ef19e795bdc89
Sahand Safizadeh <sahand.safizadeh@xebia.com>
2026-06-02 17:04:35 +0200

    step

--- examples/03-Iris/main.go
+++ examples/03-Iris/main.go
@@ -81,11 +81,11 @@ func run() (result map[string]float64, err error) {
 func prepareModel() (m *model.Model, err error) {
 	input := stream.Input()
 
-	x := stream.FC(&layers.FCConfig{Inputs: 4, Outputs: 16, Device: dev})(input)
+	x := stream.FC(&layers.FCConfig{Outputs: 16, Device: dev})(input)
 	x = stream.Relu()(x)
 	x = stream.Dropout(&layers.DropoutConfig{Rate: 0.3})(x)
 
-	x = stream.FC(&layers.FCConfig{Inputs: 16, Outputs: 3, Device: dev})(x)
+	x = stream.FC(&layers.FCConfig{Outputs: 3, Device: dev})(x)
 	output := stream.Softmax(&activations.SoftmaxConfig{Dim: 1})(x)
 
 	/* -------------------- */

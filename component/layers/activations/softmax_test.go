2a87d007fa01f00d2211af09cf0403ea9a4fd3d5
Sahand Safizadeh <sahand.safizadeh@xebia.com>
2026-06-29 15:44:49 +0200

    step

--- component/layers/activations/softmax_test.go
+++ component/layers/activations/softmax_test.go
@@ -1,6 +1,7 @@
 package activations_test
 
 import (
+	"math"
 	"testing"
 
 	"github.com/sahandsafizadeh/qeep/component/layers/activations"
@@ -113,6 +114,149 @@ func TestSoftmax(t *testing.T) {
 			}
 
 			exp, err := tensor.Full([]int{4, 8, 6}, 0.125, &tensor.Config{Device: dev})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if eq, err := act.Equals(exp); err != nil {
+				t.Fatal(err)
+			} else if !eq {
+				t.Fatal("expected tensors to be equal")
+			}
+		})
+
+		t.Run("[4] uniform large-positive input (1e300) with Dim=0 / Forward() / returns uniform 0.25 without overflow", func(t *testing.T) {
+			activation, err := activations.NewSoftmax(&activations.SoftmaxConfig{Dim: 0})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			x, err := tensor.Full([]int{4}, 1e300, &tensor.Config{Device: dev})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			act, err := activation.Forward(x)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			exp, err := tensor.Full([]int{4}, 0.25, &tensor.Config{Device: dev})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if eq, err := act.Equals(exp); err != nil {
+				t.Fatal(err)
+			} else if !eq {
+				t.Fatal("expected tensors to be equal")
+			}
+		})
+
+		t.Run("[4] uniform large-negative input (-1e300) with Dim=0 / Forward() / returns uniform 0.25 without underflow", func(t *testing.T) {
+			activation, err := activations.NewSoftmax(&activations.SoftmaxConfig{Dim: 0})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			x, err := tensor.Full([]int{4}, -1e300, &tensor.Config{Device: dev})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			act, err := activation.Forward(x)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			exp, err := tensor.Full([]int{4}, 0.25, &tensor.Config{Device: dev})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if eq, err := act.Equals(exp); err != nil {
+				t.Fatal(err)
+			} else if !eq {
+				t.Fatal("expected tensors to be equal")
+			}
+		})
+
+		t.Run("[4] input [1000, 0, 0, 0] with Dim=0 / Forward() / returns [1, 0, 0, 0] despite overflow in naive exp", func(t *testing.T) {
+			activation, err := activations.NewSoftmax(&activations.SoftmaxConfig{Dim: 0})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			x, err := tensor.Of([]float64{1000., 0., 0., 0.}, &tensor.Config{Device: dev})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			act, err := activation.Forward(x)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			exp, err := tensor.Of([]float64{1., 0., 0., 0.}, &tensor.Config{Device: dev})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if eq, err := act.Equals(exp); err != nil {
+				t.Fatal(err)
+			} else if !eq {
+				t.Fatal("expected tensors to be equal")
+			}
+		})
+
+		t.Run("[3,4] uniform large-positive input (1e308) with Dim=1 / Forward() / returns [3,4] tensor of 0.25 without overflow", func(t *testing.T) {
+			activation, err := activations.NewSoftmax(&activations.SoftmaxConfig{Dim: 1})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			x, err := tensor.Full([]int{3, 4}, 1e308, &tensor.Config{Device: dev})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			act, err := activation.Forward(x)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			exp, err := tensor.Full([]int{3, 4}, 0.25, &tensor.Config{Device: dev})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if eq, err := act.Equals(exp); err != nil {
+				t.Fatal(err)
+			} else if !eq {
+				t.Fatal("expected tensors to be equal")
+			}
+		})
+
+		t.Run("[4] input [900, 901, 902, 903] with Dim=0 / Forward() / returns same result as softmax of [-3, -2, -1, 0] despite all values overflowing in naive exp", func(t *testing.T) {
+			activation, err := activations.NewSoftmax(&activations.SoftmaxConfig{Dim: 0})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			x, err := tensor.Of([]float64{900., 901., 902., 903.}, &tensor.Config{Device: dev})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			act, err := activation.Forward(x)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			e0, e1, e2, e3 := math.Exp(-3.), math.Exp(-2.), math.Exp(-1.), math.Exp(0.)
+			s := e0 + e1 + e2 + e3
+
+			exp, err := tensor.Of([]float64{e0 / s, e1 / s, e2 / s, e3 / s}, &tensor.Config{Device: dev})
 			if err != nil {
 				t.Fatal(err)
 			}

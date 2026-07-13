1e9ebc96b09ecc32ddd0c3e666a7ee13f421b471
Sahand Safizadeh <sahand.safizadeh@xebia.com>
2026-07-13 11:42:54 +0200

    step

--- tensor/internal/cudatensor/cuda_c/types.h
+++ tensor/internal/cudatensor/cuda_c/types.h
@@ -5,23 +5,38 @@
 
 #define MAX_DIMS 6
 
-typedef struct Range
-{
-    int from;
-    int to;
-} Range;
-
-typedef struct CudaData
-{
-    double *arr;
-    size_t size;
-} CudaData;
-
 typedef struct DimArr
 {
     int arr[MAX_DIMS];
     size_t size;
 } DimArr;
+
+typedef struct CUDAView
+{
+    int ofst;
+    DimArr strd;
+    DimArr dims;
+} CUDAView;
+
+typedef struct CUDAData
+{
+    double *arr;
+    size_t size;
+} CUDAData;
+
+typedef struct CUDATensor
+{
+    CUDAView view;
+    CUDAData data;
+} CUDATensor;
+
+/* ---------- range ---------- */
+
+typedef struct Range
+{
+    int from;
+    int to;
+} Range;
 
 typedef struct RangeArr
 {

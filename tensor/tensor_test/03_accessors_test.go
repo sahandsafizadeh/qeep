426cee7cc29662381447c2ce6436f828679e1eff
Sahand Safizadeh <sahand.safizadeh@xebia.com>
2026-09-03 09:41:54 +0200

    step

--- tensor/tensor_test/03_accessors_test.go
+++ tensor/tensor_test/03_accessors_test.go
@@ -88,24 +88,6 @@ func TestShape(t *testing.T) {
 	})
 }
 
-func TestDevice(t *testing.T) {
-	tensor.RunTestLogicOnDevices(func(dev tensor.Device) {
-
-		// ============================== main paths ==============================
-
-		t.Run("Full(nil, 0) scalar tensor / Device() / returns the device it was created on", func(t *testing.T) {
-			ten, err := tensor.Full(nil, 0., &tensor.Config{Device: dev})
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if d := ten.Device(); d != dev {
-				t.Fatalf("expected tensor's device to be (%s), got (%s)", dev, d)
-			}
-		})
-	})
-}
-
 func TestNElems(t *testing.T) {
 	tensor.RunTestLogicOnDevices(func(dev tensor.Device) {
 

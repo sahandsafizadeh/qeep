d4b4e4e1993f94656d3077db8247c5e079ff7ddc
Sahand Safizadeh <sahand.safizadeh@xebia.com>
2026-09-25 15:34:02 +0200

    step

--- tensor/internal/gradtrack/types.go
+++ tensor/internal/gradtrack/types.go
@@ -5,6 +5,7 @@ import "github.com/sahandsafizadeh/qeep/tensor/internal/core"
 type gctxTensor interface {
 	core.Tensor
 	GradientContext() *GradContext
+	ResetGradContext(tracked bool)
 }
 
 type GradContext struct {
